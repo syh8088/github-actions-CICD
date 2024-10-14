523967a9e50fb76d0797ec3cd9e07512e47f1501
syh8088 <syh8088@gmail.com>
2024-10-14 16:29:46 +0900

    Create TestControllerTest.java

---
+++ springboot-CICD/src/test/java/com/springbootcicd/TestControllerTest.java
@@ -0,0 +1,16 @@
+package com.springbootcicd;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TestControllerTest {
+
+    @Test
+    public void test() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
